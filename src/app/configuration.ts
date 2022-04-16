1f7757b3a8394e8db56deb36cf309eaceb4df59f
sirmaxford <sirmaxford@gmail.com>
2022-04-16 23:32:15 +0900

    web change test

---
+++ src/app/configuration.ts
@@ -0,0 +1,69 @@
+export const Configuration =
+{
+    profileUrl:"https://avatars.githubusercontent.com/u/14171794?v=4",
+    userName:"Sam Macharia, PhD",
+    userJob:"Researcher | Educator | Engineer",
+    userLocation:"Kenya",
+    userEmail:"sammachariaphd@gmail.com",
+    //userDateOfBirth:"12/09/1998",
+    //userAddress:"Kenya",    
+    //userPhone:"xxx",
+    aboutMe:"I am a researcher, educator, and engineer interested in engineering, computing, science, and philosophy. I am currently working on a biomechatronics-related project. I’m looking to collaborate on biomechatronics and artificial life research. You may find my work here [ https://orcid.org/0000-0002-7326-7502 ].",
+};
+
+//Personal Informations
+
+export const Skills  = 
+[
+    "Research","Embedded C/C++","Python","Rust","Javascript","Automation and Biomechatronics"
+];
+
+//Max skills limit is 6 . If you add more than 6 skill , you may be disappointed.
+
+export const JobExperience  = 
+[
+    {company : "Gifu University" , rank : "Reserch Assistant" , date:"10/2018 - 06/2022" , description:"Research in Electronics and Information Systems Engineering Division, Department of Electrical, Electronics, and Computer Engineering, Graduate School of Engineering."},
+    {company : "Dedan Kimathi University of Technology" , rank : "Graduate Assistant" , date:"05/2018 - 10/2018" , description:"Assisting Mechatronic Engineering Department"}
+];
+
+//Max job experiences limit is 3 . If you add more than 3 skill , you may be disappointed.
+
+export const Education  = 
+[
+    { school : "Gifu University" , date:"2018 - 2022" , description:"Ph.D. Engineering (Electronics and Information Systems Division)"},
+    { school : "Dedan Kimathi University of Technology" , date:"2016 - 2018" , description:"M.Sc. Advanced Manufacturing and Automation Engineering"}
+];
+
+//Max education limit is 3 . If you add more than 3 skill , you may be disappointed.
+
+export const Services  = 
+[
+    { serviceName : "Data Science" ,  description:"Data analysis and machine learning."},
+    { serviceName : "Research"  , description:"Idea formulation, critical analysis, and technical writing."},
+    { serviceName : "Simulation"  , description:"Science modelling and simulation."},
+    { serviceName : "Embedded Systems"  , description:"Embedded C/C++ programming and IoT implementation."},
+    { serviceName : "Automation" ,  description:"Engineering process automation."}
+];
+
+//Max services limit is 6 . If you add more than 6 skill , you may be disappointed.
+
+export const Testimonials  = 
+[
+    { name : "David Kartuzinski" , company:"Erxes" ,  description:"We were frantically trying to prototype our latest product before a deadline and Sam was brilliant. From concept drawings alone Sam built out our full prototype for 3D printing in a matter of hours. We were able to iterate and reprint and correct the prototype over the course of several days and he met our impossible deadline." , review:85},
+    { name : "Duane Dennis" , company:"Commercializing Hemp" ,  description:"Sam is great at analyzing projects and making quick solutions to complex systems. All good results working with Sam!" , review:80},
+    { name : "Maurice Southern" , company:"Moris Group" ,  description:"We here at the Moris Group recommend Samuel Macharia to be given a thorough look,as founder at embedded systems as he has worked on our LBP system he has shown great knowledge and has worked in a timely fashion we would recommend him to others" , review:79},
+    { name : "Parth Panchal" , company:"Linkedin" ,  description:"Sam is honest, punctual in his work and goal. I worked with sam via online. He has very good Embedded Systems knowledge & skills.I highly recommend him for the Job and wishing the best for endeavor." , review:99}
+
+];
+
+//Max testimonials limit is 4 . If you add more than 4 skill , you may be disappointed.
+
+export const SocialMedia =
+{
+    github:"https://github.com/sammachariaphd",
+    instagram:"https://instagram.com/sammachariaphd",
+    twitter:"https://twitter.com/sammachariaphd",
+    linkedin:"https://www.linkedin.com/in/sammachariaphd",
+    medium:"https://sammachariaphd.medium.com",
+    facebook:"https://www.facebook.com/SamMachariaPhD"
+}
